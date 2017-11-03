bb2e8b631de24bf700f09572ba748156720ad69a
Garrett-Thomas <league@WTS5.attlocal.net>
2017-11-03 16:38:05 -0700

    testDay

--- src/CodingExercise1.java
+++ src/CodingExercise1.java
@@ -13,6 +13,39 @@ public class CodingExercise1 {
    	 // 2. set the pen width to 10
 
    	 // 1. make the Robot draw a shape
+   	 Robot droid = new Robot();
+   	 droid.setPenWidth(10);
+   	 String c = JOptionPane.showInputDialog("What color would you like the robot to draw?");
+   	 if (c.equals("green")) {
+   		 droid.setPenColor(Color.GREEN);
+		
+	}
+   	 if (c.equals("Green")) {
+   		 droid.setPenColor(Color.GREEN);
+		
+	}
+   	 if (c.equals("red")) {
+		droid.setPenColor(Color.RED);
+	}
+   	 if (c.equals("Red")) {
+		droid.setPenColor(Color.RED);
+	}
+   	 if (c.equals("blue")) {
+		droid.setPenColor(Color.BLUE);
+	}
+   	 if (c.equals("Blue")) {
+		droid.setPenColor(Color.BLUE);
+	}
+   	 droid.penDown();
+   	 droid.setSpeed(200);
+   	 for (int i = 0; i < 13; i++) {
+		droid.turn(10);
+		droid.move(50);
+		droid.turn(-150);
+		droid.move(20);
+		droid.turn(30);
+	}
+   	 droid.hide();
    	 
     }
 
--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -10,8 +10,18 @@ import javax.swing.JOptionPane;
  **/
 
 public class CodingExercise2 {
+	public static void main(String[] args) {
+String f = JOptionPane.showInputDialog("How old are you");
+int age = Integer.parseInt(f);{}
+if (age>30) {
+	JOptionPane.showMessageDialog(null, "You are to old to play this game!");
 
 }
+int ans = (2017-age);
+JOptionPane.showMessageDialog(null, "You were born in " + ans);
+}
+}
+//get year by 2017-age
 
 
 
